cfb03c5794b4d78e8f6ceb1e99edfbc5bb692135
AbhijithB141993 <99083516+AbhijithB141993@users.noreply.github.com>
2022-03-15 14:26:07 +0530

    R1

---
+++ src/com/greatlearning/model/SuperDepartment.java
@@ -0,0 +1,19 @@
+package com.greatlearning.model;
+public class SuperDepartment {
+	
+	 public String departmentName() {
+		 return("Super Department");}
+public String getTodaysWork() {
+	 return("No Work as of now");}
+public String getWorkDeadline() {
+	 return("Nill");}
+public String isTodayHoliday() {
+	 return("Today is not a Holiday");
+
+
+}
+
+
+	
+	
+}
---
+++ src/com/greatlearning/model/TechDepartment.java
@@ -0,0 +1,15 @@
+package com.greatlearning.model;
+
+public class TechDepartment extends SuperDepartment {
+
+	public TechDepartment() {
+	}
+		public String departmentName() {
+			 return("Tech Department");}
+	public String getTodaysWork() {
+		 return("Complete your documents Submission");}
+	public String getWorkDeadline() {
+		 return("Complete by EOD ");}
+	
+	}
+
